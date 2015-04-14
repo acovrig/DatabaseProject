c0e53bcac3e2841ec829eaebe8bffa9dbf46d786
Austin Covrig <accovrig@gmail.com>
2015-04-14 12:14:13 -0400

    Switched to SQL Server (from MySQL)

--- planning/Database_Project_create.sql
+++ planning/Database_Project_create.sql
@@ -1,75 +1,105 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Script type: create
--- Scope: [tables, references, sequences, views, procedures]
--- Generated at Thu Mar 19 16:36:43 UTC 2015
+-- Last modification date: 2015-04-14 15:34:51.841
 
 
 
 
 -- tables
--- Table Accounts
+-- Table: Accounts
 CREATE TABLE Accounts (
-    AcctID int    NOT NULL ,
-    type varchar(50)    NOT NULL ,
-    BankID int    NOT NULL ,
-    UNIQUE INDEX BankIDAcct (BankID),
+    AcctID int  NOT NULL,
+    type varchar(50)  NOT NULL,
+    BankID int  NOT NULL,
+    CONSTRAINT BankIDAcct UNIQUE (BankID),
     CONSTRAINT Accounts_pk PRIMARY KEY (AcctID)
-);
+)
+;
 
--- Table Banks
+
+
+
+
+-- Table: Banks
 CREATE TABLE Banks (
-    BankID int    NOT NULL ,
-    Name varchar(200)    NOT NULL ,
-    Address varchar(255)    NOT NULL ,
-    Phone bigint    NOT NULL ,
+    BankID int  NOT NULL,
+    Name varchar(200)  NOT NULL,
+    Street varchar(255)  NOT NULL,
+    City varchar(50)  NOT NULL,
+    ZIP int  NOT NULL,
+    Phone bigint  NOT NULL,
     CONSTRAINT Banks_pk PRIMARY KEY (BankID)
-);
+)
+;
 
--- Table Categories
+
+
+
+
+-- Table: Categories
 CREATE TABLE Categories (
-    CategoryID int    NOT NULL ,
-    Name varchar(50)    NOT NULL ,
-    TaxDeductible bool    NOT NULL ,
-    UNIQUE INDEX Categories (Name),
+    CategoryID int  NOT NULL,
+    Name varchar(50)  NOT NULL,
+    TaxDeductible bit  NOT NULL,
+    CONSTRAINT Categories UNIQUE (Name),
     CONSTRAINT Categories_pk PRIMARY KEY (CategoryID)
-);
+)
+;
 
--- Table Payees
+
+
+
+
+-- Table: Loans
+CREATE TABLE Loans (
+    LoanID int  NOT NULL,
+    Name varchar(100)  NOT NULL,
+    BankID int  NOT NULL,
+    Company varchar(100)  NOT NULL,
+    CONSTRAINT Loans_pk PRIMARY KEY (LoanID)
+)
+;
+
+
+
+
+
+-- Table: Payees
 CREATE TABLE Payees (
-    Name varchar(100)    NOT NULL ,
-    LastAmount decimal(10,2)    NOT NULL ,
-    Category varchar(50)    NOT NULL ,
-    Note text    NOT NULL ,
-    `To` varchar(100)    NOT NULL ,
-    UNIQUE INDEX CategoryName (Category),
+    Name varchar(100)  NOT NULL,
+    LastAmount decimal(10,2)  NOT NULL,
+    Category varchar(50)  NOT NULL,
+    Note text  NOT NULL,
+    "To" varchar(100)  NOT NULL,
+    CONSTRAINT CategoryName UNIQUE (Category),
     CONSTRAINT Payees_pk PRIMARY KEY (Name)
-);
+)
+;
 
--- Table Tithe
-CREATE TABLE Tithe (
-    TransID int    NOT NULL ,
-    Tithe decimal(10,2)    NOT NULL ,
-    Paid bool    NOT NULL ,
-    UNIQUE INDEX Tithe_ak_1 (TransID),
-    CONSTRAINT Tithe_pk PRIMARY KEY (TransID)
-);
 
--- Table Transactions
+
+
+
+-- Table: Transactions
 CREATE TABLE Transactions (
-    TransID int    NOT NULL ,
-    Amount decimal(10,2)    NOT NULL ,
-    AcctID int    NOT NULL ,
-    `To` varchar(100)    NOT NULL ,
-    `From` varchar(100)    NOT NULL ,
-    CategoryID int    NOT NULL ,
-    Note text    NOT NULL ,
-    Reconciled bool    NOT NULL ,
-    CheckNum bool    NOT NULL ,
-    Image blob    NOT NULL ,
-    UNIQUE INDEX AcctID (AcctID),
-    UNIQUE INDEX CategoryCat (CategoryID),
+    TransID int  NOT NULL,
+    Amount decimal(10,2)  NOT NULL,
+    AcctID int  NOT NULL,
+    "To" varchar(100)  NOT NULL,
+    "From" varchar(100)  NOT NULL,
+    CategoryID int  NOT NULL,
+    Note text  NOT NULL,
+    Reconciled bit  NOT NULL,
+    CheckNum bit  NOT NULL,
+    Image image  NOT NULL,
+    CONSTRAINT AcctID UNIQUE (AcctID),
+    CONSTRAINT CategoryCat UNIQUE (CategoryID),
     CONSTRAINT Transactions_pk PRIMARY KEY (TransID)
-);
+)
+;
+
+
+
+
 
 
 
@@ -79,36 +109,52 @@ CREATE TABLE Transactions (
 -- Reference:  AcctID (table: Transactions)
 
 
-ALTER TABLE Transactions ADD CONSTRAINT AcctID FOREIGN KEY AcctID (AcctID)
+ALTER TABLE Transactions ADD CONSTRAINT AcctID 
+    FOREIGN KEY (AcctID)
     REFERENCES Accounts (AcctID)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT;
+;
+
 -- Reference:  BankID (table: Accounts)
 
 
-ALTER TABLE Accounts ADD CONSTRAINT BankID FOREIGN KEY BankID (BankID)
+ALTER TABLE Accounts ADD CONSTRAINT BankID 
+    FOREIGN KEY (BankID)
     REFERENCES Banks (BankID)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT;
+;
+
+-- Reference:  Banks_Loans (table: Loans)
+
+
+ALTER TABLE Loans ADD CONSTRAINT Banks_Loans 
+    FOREIGN KEY (BankID)
+    REFERENCES Banks (BankID)
+;
+
 -- Reference:  CategoriesPay (table: Payees)
 
 
-ALTER TABLE Payees ADD CONSTRAINT CategoriesPay FOREIGN KEY CategoriesPay (Category)
+ALTER TABLE Payees ADD CONSTRAINT CategoriesPay 
+    FOREIGN KEY (Category)
     REFERENCES Categories (Name)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT;
+;
+
 -- Reference:  CategoriesTran (table: Transactions)
 
 
-ALTER TABLE Transactions ADD CONSTRAINT CategoriesTran FOREIGN KEY CategoriesTran (CategoryID)
+ALTER TABLE Transactions ADD CONSTRAINT CategoriesTran 
+    FOREIGN KEY (CategoryID)
     REFERENCES Categories (CategoryID)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT;
--- Reference:  Tithe_Transactions (table: Tithe)
+;
+
+-- Reference:  Payees_Loans (table: Loans)
 
 
-ALTER TABLE Tithe ADD CONSTRAINT Tithe_Transactions FOREIGN KEY Tithe_Transactions (TransID)
-    REFERENCES Transactions (TransID);
+ALTER TABLE Loans ADD CONSTRAINT Payees_Loans 
+    FOREIGN KEY (Company)
+    REFERENCES Payees (Name)
+;
+
+
 
 
 
