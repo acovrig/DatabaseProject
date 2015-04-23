025b6b190a92730d378fd39e14872c08d74ba12d
Austin Covrig <accovrig@gmail.com>
2015-04-22 20:29:48 -0400

    Added PRIMARY KEYS and simplified syntax

--- planning/Database_Project_create.sql
+++ planning/Database_Project_create.sql
@@ -1,86 +1,64 @@
 -- Created by Vertabelo (http://vertabelo.com)
 -- Last modification date: 2015-04-14 15:34:51.841
 
-
-
+USE master
+IF EXISTS(select * from sys.databases where name='Money')
+	DROP DATABASE Money
+	CREATE DATABASE Money
+GO
+USE Money
+GO
 
 -- tables
--- Table: Accounts
-CREATE TABLE Accounts (
-    AcctID int  NOT NULL,
-    type varchar(50)  NOT NULL,
-    BankID int  NOT NULL,
-    CONSTRAINT BankIDAcct UNIQUE (BankID),
-    CONSTRAINT Accounts_pk PRIMARY KEY (AcctID)
-)
-;
-
-
-
-
-
 -- Table: Banks
-CREATE TABLE Banks (
-    BankID int  NOT NULL,
+CREATE TABLE [Banks] (
+    BankID int  NOT NULL PRIMARY KEY,
     Name varchar(200)  NOT NULL,
     Street varchar(255)  NOT NULL,
     City varchar(50)  NOT NULL,
     ZIP int  NOT NULL,
-    Phone bigint  NOT NULL,
-    CONSTRAINT Banks_pk PRIMARY KEY (BankID)
-)
-;
+    Phone bigint  NOT NULL
+);
 
-
-
-
+-- Table: Accounts
+CREATE TABLE [Accounts] (
+    AcctID int  NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    type varchar(50)  NOT NULL,
+    BankID int  NOT NULL,
+    CONSTRAINT BankID FOREIGN KEY (BankID) REFERENCES Banks (BankID)
+);
 
 -- Table: Categories
-CREATE TABLE Categories (
-    CategoryID int  NOT NULL,
+CREATE TABLE [Category] (
+    CategoryID int  NOT NULL IDENTITY(1,1) PRIMARY KEY,
     Name varchar(50)  NOT NULL,
     TaxDeductible bit  NOT NULL,
-    CONSTRAINT Categories UNIQUE (Name),
-    CONSTRAINT Categories_pk PRIMARY KEY (CategoryID)
-)
-;
-
-
-
-
-
--- Table: Loans
-CREATE TABLE Loans (
-    LoanID int  NOT NULL,
-    Name varchar(100)  NOT NULL,
-    BankID int  NOT NULL,
-    Company varchar(100)  NOT NULL,
-    CONSTRAINT Loans_pk PRIMARY KEY (LoanID)
-)
-;
-
-
-
-
+    CONSTRAINT Categories UNIQUE (Name)
+);
 
 -- Table: Payees
-CREATE TABLE Payees (
-    Name varchar(100)  NOT NULL,
+CREATE TABLE [Payees] (
+    Name varchar(100)  NOT NULL UNIQUE,
     LastAmount decimal(10,2)  NOT NULL,
     Category varchar(50)  NOT NULL,
     Note text  NOT NULL,
     "To" varchar(100)  NOT NULL,
     CONSTRAINT CategoryName UNIQUE (Category),
-    CONSTRAINT Payees_pk PRIMARY KEY (Name)
-)
-;
+    CONSTRAINT CategoriesPay FOREIGN KEY (Category) REFERENCES Category (Name)
+);
 
-
-
-
+-- Table: Loans
+CREATE TABLE [Loans] (
+    LoanID int  NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    Name varchar(100)  NOT NULL,
+    BankID int  NOT NULL,
+    Company varchar(100)  NOT NULL,
+    CONSTRAINT Payees_Loans FOREIGN KEY (Company) REFERENCES Payees (Name),
+    CONSTRAINT Banks_Loans FOREIGN KEY (BankID) REFERENCES Banks (BankID)
+);
 
 -- Table: Transactions
-CREATE TABLE Transactions (
+CREATE TABLE [Transactions] (
     TransID int  NOT NULL,
     Amount decimal(10,2)  NOT NULL,
     AcctID int  NOT NULL,
@@ -91,72 +69,8 @@ CREATE TABLE Transactions (
     Reconciled bit  NOT NULL,
     CheckNum bit  NOT NULL,
     Image image  NOT NULL,
-    CONSTRAINT AcctID UNIQUE (AcctID),
-    CONSTRAINT CategoryCat UNIQUE (CategoryID),
-    CONSTRAINT Transactions_pk PRIMARY KEY (TransID)
-)
-;
-
-
-
-
-
-
-
-
-
--- foreign keys
--- Reference:  AcctID (table: Transactions)
-
-
-ALTER TABLE Transactions ADD CONSTRAINT AcctID 
-    FOREIGN KEY (AcctID)
-    REFERENCES Accounts (AcctID)
-;
-
--- Reference:  BankID (table: Accounts)
-
-
-ALTER TABLE Accounts ADD CONSTRAINT BankID 
-    FOREIGN KEY (BankID)
-    REFERENCES Banks (BankID)
-;
-
--- Reference:  Banks_Loans (table: Loans)
-
-
-ALTER TABLE Loans ADD CONSTRAINT Banks_Loans 
-    FOREIGN KEY (BankID)
-    REFERENCES Banks (BankID)
-;
-
--- Reference:  CategoriesPay (table: Payees)
-
-
-ALTER TABLE Payees ADD CONSTRAINT CategoriesPay 
-    FOREIGN KEY (Category)
-    REFERENCES Categories (Name)
-;
-
--- Reference:  CategoriesTran (table: Transactions)
-
-
-ALTER TABLE Transactions ADD CONSTRAINT CategoriesTran 
-    FOREIGN KEY (CategoryID)
-    REFERENCES Categories (CategoryID)
-;
-
--- Reference:  Payees_Loans (table: Loans)
-
-
-ALTER TABLE Loans ADD CONSTRAINT Payees_Loans 
-    FOREIGN KEY (Company)
-    REFERENCES Payees (Name)
-;
-
-
-
-
+    CONSTRAINT AcctIDConstraint FOREIGN KEY (AcctID) REFERENCES Accounts (AcctID),
+    CONSTRAINT CategoriesTran FOREIGN KEY (CategoryID) REFERENCES Category (CategoryID)
+);
 
 -- End of file.
-
