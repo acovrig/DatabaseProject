060add5084f2f04723036a878b873266f1907165
Austin Covrig <accovrig@gmail.com>
2015-04-22 20:30:42 -0400

    Added base code (met with Dr.A)

---
+++ DatabaseProject/DatabaseProject/Controllers/Default1Controller.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseProject.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        private MoneyEntities db = new MoneyEntities();
+
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View(db.Banks.ToList());
+        }
+
+        //
+        // GET: /Default1/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Bank bank = db.Banks.Find(id);
+            if (bank == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bank);
+        }
+
+        //
+        // GET: /Default1/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Default1/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Bank bank)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Banks.Add(bank);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bank);
+        }
+
+        //
+        // GET: /Default1/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Bank bank = db.Banks.Find(id);
+            if (bank == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bank);
+        }
+
+        //
+        // POST: /Default1/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Bank bank)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bank).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bank);
+        }
+
+        //
+        // GET: /Default1/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Bank bank = db.Banks.Find(id);
+            if (bank == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bank);
+        }
+
+        //
+        // POST: /Default1/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bank bank = db.Banks.Find(id);
+            db.Banks.Remove(bank);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
