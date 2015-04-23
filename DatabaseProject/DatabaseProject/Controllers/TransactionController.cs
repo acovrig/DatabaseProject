33025cb107e0ed8d99631a475366941659159453
Austin Covrig <accovrig@gmail.com>
2015-04-22 23:20:41 -0400

    Fixed broken debug code/typo

--- DatabaseProject/DatabaseProject/Controllers/TransactionController.cs
+++ DatabaseProject/DatabaseProject/Controllers/TransactionController.cs
@@ -54,17 +54,6 @@ namespace DatabaseProject.Controllers
             if (ModelState.IsValid)
             {
                 db.Transactions.Add(transaction);
-
-
-                foreach (var ve in eve.ValidationErrors)
-                {
-                    Console.WriteLine("- Property: \"{0}\", Value: \"{1}\", Error: \"{2}\"",
-                        ve.PropertyName,
-                        eve.Entry.CurrentValues.GetValue<object>(ve.PropertyName),
-                        ve.ErrorMessage);
-                }
-
-
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
