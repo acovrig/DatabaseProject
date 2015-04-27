b1b9e16810c1d0d64ac9a944f4470fde606228f5
Austin Covrig <accovrig@gmail.com>
2015-04-27 17:54:11 -0400

    Fixed #27

---
+++ DatabaseProject/DatabaseProject/RefurringTransactionMod.cs
@@ -0,0 +1,21 @@
+﻿namespace DatabaseProject
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class RecurringTransaction
+    {
+        public String Value {
+            get {
+                if (ExpectedAmount < 0)
+                {
+                    return "-$" + (ExpectedAmount * -1);
+                }
+                else
+                {
+                    return "$" + ExpectedAmount;
+                }
+            }
+        }
+    }
+}
