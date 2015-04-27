4007329ee0ff00f8a45cb7f2d2d5f126cbfeb90e
Austin Covrig <accovrig@gmail.com>
2015-04-27 17:54:11 -0400

    Fixed #27

---
+++ DatabaseProject/DatabaseProject/BankAccountMod.cs
@@ -0,0 +1,15 @@
+﻿namespace DatabaseProject
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class BankAccount
+    {
+        public String Balance {
+            get
+            {
+                return "testing";
+            }
+        }
+    }
+}
