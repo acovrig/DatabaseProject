6dcdd2bf0e9552e1fc59a37b461f3262eb172c99
Austin Covrig <accovrig@gmail.com>
2015-04-28 12:20:29 -0400

    Fixed #29

---
+++ DatabaseProject/DatabaseProject/BankMod.cs
@@ -0,0 +1,12 @@
+﻿namespace DatabaseProject
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public partial class Bank
+    {
+        [DisplayFormat(DataFormatString = "{0:(###) ###-####}")]
+        public long FormattedPhone { get { return Phone; } }
+    }
+}
